61b5c2cce24180ed99025c5cb3559bb2b6990d44
Captain Crypto <tamir@crypto-knight.tech>
2018-11-29 14:17:27 +0200

    PAI-BOT Module comments

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+/*
+ Export module to the bot engine
+ Author       : Ron Fridman
+ Date Created : 08/11/2018
+ Copyright PAI-TECH 2018, all right reserved
+
+ *      This program is free software; you can redistribute it and/or
+ *		modify it under the terms of the GNU General Public License
+ *		as published by the Free Software Foundation; either version
+ *		3 of the License, or (at your option) any later version.
+  */
+
 const Module = require('./src/my-module/pcm-my-module');
 
 
---
+++ readme.md
@@ -0,0 +1,14 @@
+# PAI-BOT Base module
+
+ > PAI Code Base module
+ > Description     : use this project to write your own module
+ > Author          : Ron Fridman
+ > Date Created    : 19/11/2018
+
+ > Copyright **PAI-TECH** 2018, all right reserved
+
+
+ # Description
+
+ This base project is used for developers who wants to develop an PAI-BOT module
+ the can be learned by the PAI-BOT.
